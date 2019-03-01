f46fd00e7e742f7a6204b15a1a19fc825a291694
vladimir.morozov <vladimir.morozov@totalobjects.spb.ru>
2019-03-01 09:50:14 +0300

    add border comparer

---
+++ OOXMLComparer/OOXMLComparer.Test/BorderComparerTests.cs
@@ -0,0 +1,49 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using NUnit.Framework;
+using OOXMLComparer.Properties;
+
+namespace OOXMLComparer.Test
+{
+    public class BorderComparerTests
+    {
+        [Test]
+        public void BorderComparerTest()
+        {
+            var a = new Border { Val = BorderValues.Single };
+            var b = new Border { Val = BorderValues.Single };
+            var borderComparer = new BorderComparer();
+            Assert.IsTrue(borderComparer.Compare(a, b));
+        }
+
+        [Test]
+        public void BorderComparerWrongTest()
+        {
+            var a = new Border { Val = BorderValues.Single };
+            var b = new Border { Val = BorderValues.Sombrero };
+            var BorderComparer = new BorderComparer();
+            Assert.IsFalse(BorderComparer.Compare(a, b));
+        }
+
+        [Test]
+        public void BorderComparerNullTest()
+        {
+            var a = new Border { Val = BorderValues.None };
+            var a1 = new Border { Val = BorderValues.Nil };
+            var BorderComparer = new BorderComparer();
+            Assert.IsTrue(BorderComparer.Compare(a, null));
+            Assert.IsTrue(BorderComparer.Compare(null, a));
+            Assert.IsTrue(BorderComparer.Compare(a1, null));
+            Assert.IsTrue(BorderComparer.Compare(null, a1));
+            Assert.IsTrue(BorderComparer.Compare(null, null));
+        }
+
+        [Test]
+        public void BorderComparerNullWrongTest()
+        {
+            var a = new Border { Val = BorderValues.Single };
+            var BorderComparer = new BorderComparer();
+            Assert.IsFalse(BorderComparer.Compare(a, null));
+            Assert.IsFalse(BorderComparer.Compare(null, a));
+        }
+    }
+}
---
+++ OOXMLComparer/OOXMLComparer/Properties/BorderComparer.cs
@@ -0,0 +1,28 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace OOXMLComparer.Properties
+{
+    public class BorderComparer : IOpenXmlElementComparer<Border>
+    {
+        public bool Compare(Border a, Border b)
+        {
+            if (a == null && b == null)
+            {
+                return true;
+            }
+            if (a == null && (b.Val == BorderValues.None || b.Val == BorderValues.Nil))
+            {
+                return true;
+            }
+            if (b == null && (a.Val == BorderValues.None || a.Val == BorderValues.Nil))
+            {
+                return true;
+            }
+            if (a == null || b == null)
+            {
+                return false;
+            }
+            return Equals(a.Val, b.Val);
+        }
+    }
+}
