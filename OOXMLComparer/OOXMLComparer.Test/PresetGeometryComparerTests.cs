47111d1da744fbe1033ad0986d90c4d53b26f9fa
vladimir.morozov <vladimir.morozov@totalobjects.spb.ru>
2019-03-05 11:16:37 +0300

    Compare preset geometry

---
+++ OOXMLComparer/OOXMLComparer.Test/PresetGeometryComparerTests.cs
@@ -0,0 +1,40 @@
+﻿using DocumentFormat.OpenXml.Drawing;
+using NUnit.Framework;
+using OOXMLComparer.Drawing;
+
+namespace OOXMLComparer.Test
+{
+    public class PresetGeometryComparerTests
+    {
+        [Test]
+        public void PresetGeometryComparerTest()
+        {
+            var a = new PresetGeometry { Preset = ShapeTypeValues.Rectangle };
+            var b = new PresetGeometry { Preset = ShapeTypeValues.Rectangle };
+            Assert.IsTrue(new PresetGeometryComparer(a, b).Compare());
+            Assert.IsTrue(new PresetGeometryComparer(new PresetGeometry(), new PresetGeometry()).Compare());
+
+            a = new PresetGeometry { Preset = ShapeTypeValues.Rectangle, AdjustValueList = new AdjustValueList() };
+            b = new PresetGeometry { Preset = ShapeTypeValues.Rectangle, AdjustValueList = new AdjustValueList() };
+            Assert.IsTrue(new PresetGeometryComparer(a, b).Compare());
+        }
+
+        public void PresetGeometryComparerWrongTest()
+        {
+            var a = new PresetGeometry { Preset = ShapeTypeValues.Rectangle };
+            var b = new PresetGeometry();
+            var c = new PresetGeometry { Preset = ShapeTypeValues.Ribbon };
+            Assert.IsFalse(new PresetGeometryComparer(a, b).Compare());
+            Assert.IsFalse(new PresetGeometryComparer(a, c).Compare());
+        }
+
+        public void PresetGeometryComparerNullTest()
+        {
+            var a = new PresetGeometry { Preset = ShapeTypeValues.Rectangle };
+            Assert.IsFalse(new PresetGeometryComparer(a, null).Compare());
+            Assert.IsFalse(new PresetGeometryComparer(null, a).Compare());
+            Assert.IsTrue(new PresetGeometryComparer(null, null).Compare());
+        }
+
+    }
+}
---
+++ OOXMLComparer/OOXMLComparer/Drawing/PresetGeometryComparer.cs
@@ -0,0 +1,34 @@
+﻿using DocumentFormat.OpenXml.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOXMLComparer.Drawing
+{
+    public class PresetGeometryComparer : OpenXmlElementComparer<PresetGeometry>
+    {
+        public PresetGeometryComparer(PresetGeometry a, PresetGeometry b) : base(a, b)
+        {
+        }
+        public override bool Compare()
+        {
+            if (a == null && b == null)
+            {
+                return true;
+            }
+            if (a == null && b.Preset == null && b.AdjustValueList == null)
+            {
+                return true;
+            }
+            if (b == null && a.Preset == null && a.AdjustValueList == null)
+            {
+                return true;
+            }
+            if (a == null || b == null)
+            {
+                return false;
+            }
+            return Equals(a.Preset, b.Preset) && ((a.AdjustValueList == null && b.AdjustValueList == null)||(a.AdjustValueList != null && b.AdjustValueList != null));
+        }
+    }
+}
