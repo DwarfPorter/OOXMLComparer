c7b5ce9a48c27dab87594439315938af12b17332
vladimir.morozov <vladimir.morozov@totalobjects.spb.ru>
2019-03-06 11:48:35 +0300

    Add StrechRectangle

---
+++ OOXMLComparer/OOXMLComparer.Test/StretchComparerTests.cs
@@ -0,0 +1,38 @@
+﻿using DocumentFormat.OpenXml.Drawing;
+using NUnit.Framework;
+using OOXMLComparer.Drawing;
+
+namespace OOXMLComparer.Test
+{
+    public class StretchComparerTests
+    {
+        [Test]
+        public void StretchComparerTest()
+        {
+            var a = new Stretch();
+            var b = new Stretch();
+            Assert.IsTrue(new StretchComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void StretchComparerNullTest()
+        {
+            var a = new Stretch();
+            Assert.IsTrue(new StretchComparer(a, null).Compare());
+            Assert.IsTrue(new StretchComparer(null, a).Compare());
+            Assert.IsTrue(new StretchComparer(null, null).Compare());
+        }
+
+        [Test]
+        public void StretchComparerWrongTest()
+        {
+            var a = new Stretch() { FillRectangle = new FillRectangle() };
+            var b = new Stretch();
+            Assert.IsFalse(new StretchComparer(a, b).Compare());
+            Assert.IsFalse(new StretchComparer(b, a).Compare());
+
+            b = new Stretch(new FillRectangle());
+            Assert.IsTrue(new StretchComparer(a, b).Compare());
+        }
+    }
+}
---
+++ OOXMLComparer/OOXMLComparer/Drawing/StretchComparer.cs
@@ -0,0 +1,17 @@
+﻿using DocumentFormat.OpenXml.Drawing;
+using OOXMLComparer.Helpers;
+
+namespace OOXMLComparer.Drawing
+{
+    public class StretchComparer : OpenXmlElementComparer<Stretch>
+    {
+        public StretchComparer(Stretch a, Stretch b) : base(a, b)
+        {
+        }
+
+        public override bool Compare()
+        {
+            return a.CompareNullElements(b, t => t.FillRectangle == null) ?? ((a.FillRectangle == null && b.FillRectangle == null) || (a.FillRectangle != null && b.FillRectangle != null));
+        }
+    }
+}
