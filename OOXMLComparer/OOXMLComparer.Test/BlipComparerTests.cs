da9260199cc22e3a72e7e088048c9ba8296b8e1c
vladimir.morozov <vladimir.morozov@totalobjects.spb.ru>
2019-03-07 11:05:00 +0300

    Revert "broke test"
    This reverts commit aa192da781147ceeb13e0d3401eadab8a64efb54.

--- OOXMLComparer/OOXMLComparer.Test/BlipComparerTests.cs
+++ OOXMLComparer/OOXMLComparer.Test/BlipComparerTests.cs
@@ -103,7 +103,6 @@ namespace OOXMLComparer.Test
             var a = doc1.FooterParts.First().Footer.Descendants<Blip>().First();
             var b = doc2.FooterParts.First().Footer.Descendants<Blip>().First();
             Assert.IsTrue(new BlipComparer(a, b).Compare());
-            Assert.IsFalse(true);
         }
 
         [Test]
