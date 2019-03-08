66bb0b9669d3b84e7c0c3ff0eea4a6d3cf6e83be
vladimir.morozov <vladimir.morozov@totalobjects.spb.ru>
2019-03-08 13:16:38 +0300

    test NonVisualDrawingProperties

---
+++ OOXMLComparer/OOXMLComparer.Test/NonVisualDrawingPropertiesComparerTests.cs
@@ -0,0 +1,52 @@
+﻿using DocumentFormat.OpenXml.Drawing;
+using NUnit.Framework;
+using OOXMLComparer.Drawing;
+
+namespace OOXMLComparer.Test
+{
+    public class NonVisualDrawingPropertiesComparerTests
+    {
+        [Test]
+        public void NonVisualDrawingPropertiesComparerTest()
+        {
+            var a = new NonVisualDrawingProperties();
+            var b = new NonVisualDrawingProperties();
+            Assert.IsTrue(new NonVisualDrawingPropertiesComparer(a, b).Compare());
+            a.Hidden = true;
+            b.Hidden = true;
+            Assert.IsTrue(new NonVisualDrawingPropertiesComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void NonVisualDrawingPropertiesComparerNullTest()
+        {
+            var a = new NonVisualDrawingProperties();
+            Assert.IsTrue(new NonVisualDrawingPropertiesComparer(a, null).Compare());
+            Assert.IsTrue(new NonVisualDrawingPropertiesComparer(null, a).Compare());
+            Assert.IsTrue(new NonVisualDrawingPropertiesComparer(null, null).Compare());
+        }
+
+        [Test]
+        public void NonVisualDrawingPropertiesComparerWrongTest()
+        {
+            var a = new NonVisualDrawingProperties() { Hidden = true };
+            var b = new NonVisualDrawingProperties();
+            Assert.IsFalse(new NonVisualDrawingPropertiesComparer(a, b).Compare());
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ OOXMLComparer/OOXMLComparer/Drawing/NonVisualDrawingPropertiesComparer.cs
@@ -0,0 +1,17 @@
+﻿using DocumentFormat.OpenXml.Drawing;
+using OOXMLComparer.Helpers;
+
+namespace OOXMLComparer.Drawing
+{
+    public class NonVisualDrawingPropertiesComparer : OpenXmlElementComparer<NonVisualDrawingProperties>
+    {
+        public NonVisualDrawingPropertiesComparer(NonVisualDrawingProperties a, NonVisualDrawingProperties b) : base(a, b)
+        {
+        }
+
+        public override bool Compare()
+        {
+            return a.CompareNullElements(b, t => !t.Hidden.ToBool()) ?? a.Hidden.EqualBool(b.Hidden);
+        }
+    }
+}
