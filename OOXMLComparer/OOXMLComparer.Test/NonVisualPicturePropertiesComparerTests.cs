594d3c6fd9ec364733dee01b36e576bcb2217c3a
vladimir.morozov <vladimir.morozov@totalobjects.spb.ru>
2019-03-08 13:37:49 +0300

    test NonVisualPictureDrawingProperties

---
+++ OOXMLComparer/OOXMLComparer.Test/NonVisualPicturePropertiesComparerTests.cs
@@ -0,0 +1,42 @@
+﻿using DocumentFormat.OpenXml.Drawing;
+using NUnit.Framework;
+using OOXMLComparer.Drawing;
+
+namespace OOXMLComparer.Test
+{
+    public class NonVisualPicturePropertiesComparerTests
+    {
+        [Test]
+        public void NonVisualPicturePropertiesComparerTest()
+        {
+            var a = new NonVisualPictureProperties();
+            var b = new NonVisualPictureProperties();
+            Assert.IsTrue(new NonVisualPicturePropertiesComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void NonVisualPicturePropertiesComparerHappyTest()
+        { 
+            var a = new NonVisualPictureProperties(new NonVisualPictureDrawingProperties() { PreferRelativeResize = true });
+            var b = new NonVisualPictureProperties(new NonVisualPictureDrawingProperties() { PreferRelativeResize = true });
+            Assert.IsTrue(new NonVisualPicturePropertiesComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void NonVisualPicturePropertiesComparerNullTest()
+        {
+            var a = new NonVisualPictureProperties();
+            Assert.IsTrue(new NonVisualPicturePropertiesComparer(a, null).Compare());
+            Assert.IsTrue(new NonVisualPicturePropertiesComparer(null, a).Compare());
+            Assert.IsTrue(new NonVisualPicturePropertiesComparer(null, null).Compare());
+        }
+
+        [Test]
+        public void NonVisualPicturePropertiesComparerWrongTest()
+        {
+            var a = new NonVisualPictureProperties(new NonVisualPictureDrawingProperties() { PreferRelativeResize = true });
+            var b = new NonVisualPictureProperties(new NonVisualPictureDrawingProperties() { PreferRelativeResize = false });
+            Assert.IsFalse(new NonVisualPicturePropertiesComparer(a, b).Compare());
+        }
+    }
+}
---
+++ OOXMLComparer/OOXMLComparer/Drawing/NonVisualPicturePropertiesComparer.cs
@@ -0,0 +1,17 @@
+﻿using DocumentFormat.OpenXml.Drawing;
+using OOXMLComparer.Helpers;
+
+namespace OOXMLComparer.Drawing
+{
+    public class NonVisualPicturePropertiesComparer : OpenXmlElementComparer<NonVisualPictureProperties>
+    {
+        public NonVisualPicturePropertiesComparer(NonVisualPictureProperties a, NonVisualPictureProperties b) : base(a, b)
+        {
+        }
+
+        public override bool Compare()
+        {
+            return a.CompareChildren2(b);
+        }
+    }
+}
