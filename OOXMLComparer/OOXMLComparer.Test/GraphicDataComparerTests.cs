c828f2377ff466342f21811a940b0e30fbf9a719
vladimir.morozov <vladimir.morozov@totalobjects.spb.ru>
2019-03-08 14:04:23 +0300

    test GraphicData

---
+++ OOXMLComparer/OOXMLComparer.Test/GraphicDataComparerTests.cs
@@ -0,0 +1,32 @@
+﻿using DocumentFormat.OpenXml.Drawing;
+using NUnit.Framework;
+using OOXMLComparer.Drawing;
+
+namespace OOXMLComparer.Test
+{
+    public class GraphicDataComparerTests
+    {
+        [Test]
+        public void GraphicDataComparerTest()
+        {
+            var a = new GraphicData();
+            var b = new GraphicData();
+            Assert.IsTrue(new GraphicDataComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void GraphicDataComparerWrongTest()
+        {
+            var a = new GraphicData(new DocumentFormat.OpenXml.Drawing.Pictures.Picture(
+                                        new DocumentFormat.OpenXml.Drawing.Pictures.NonVisualPictureProperties(
+                                            new DocumentFormat.OpenXml.Drawing.Pictures.NonVisualDrawingProperties(),
+                                            new DocumentFormat.OpenXml.Drawing.Pictures.NonVisualPictureDrawingProperties(
+                                                new DocumentFormat.OpenXml.Drawing.PictureLocks() { NoChangeAspect = true, NoChangeArrowheads = true })
+                                            )
+                                        )
+                                    );
+            var b = new GraphicData();
+            Assert.IsFalse(new GraphicDataComparer(a, b).Compare());
+        }
+    }
+}
---
+++ OOXMLComparer/OOXMLComparer/Drawing/GraphicDataComparer.cs
@@ -0,0 +1,17 @@
+﻿using DocumentFormat.OpenXml.Drawing;
+using OOXMLComparer.Helpers;
+
+namespace OOXMLComparer.Drawing
+{
+    public class GraphicDataComparer : OpenXmlElementComparer<GraphicData>
+    {
+        public GraphicDataComparer(GraphicData a, GraphicData b) : base(a, b)
+        {
+        }
+
+        public override bool Compare()
+        {
+            return a.CompareChildren2(b);
+        }
+    }
+}
