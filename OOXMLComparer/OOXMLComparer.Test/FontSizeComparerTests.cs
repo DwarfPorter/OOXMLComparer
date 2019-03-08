3f952d84276c8b37965fb04849e81c633071e9fa
vladimir.morozov <vladimir.morozov@totalobjects.spb.ru>
2019-03-08 09:21:44 +0300

    begin to PictureLocksComparer

--- OOXMLComparer/OOXMLComparer.Test/FontSizeComparerTests.cs
+++ OOXMLComparer/OOXMLComparer.Test/FontSizeComparerTests.cs
@@ -1,5 +1,7 @@
-﻿using DocumentFormat.OpenXml.Wordprocessing;
+﻿using DocumentFormat.OpenXml.Drawing;
+using DocumentFormat.OpenXml.Wordprocessing;
 using NUnit.Framework;
+using OOXMLComparer.Drawing;
 using OOXMLComparer.Properties;
 
 namespace OOXMLComparer.Test
@@ -26,5 +28,96 @@ namespace OOXMLComparer.Test
             Assert.IsFalse(new FontSizeComparer(a, null).Compare());
             Assert.IsFalse(new FontSizeComparer(null, b).Compare());
         }
+    }
+    public class PictureLocksComparerTests
+    {
+        [Test]
+        public void PictureLocksComparerTest()
+        {
+            var a = new PictureLocks();
+            var b = new PictureLocks();
+            Assert.IsTrue(new PictureLocksComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void PictureLocksComparer_NoChangeShapeTypeWrongTest()
+        {
+            var a = new PictureLocks() { NoChangeShapeType = true };
+            var b = new PictureLocks();
+            Assert.IsTrue(new PictureLocksComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void PictureLocksComparer_NoChangeArrowheadsWrongTest()
+        {
+            var a = new PictureLocks() { NoChangeArrowheads = true };
+            var b = new PictureLocks();
+            Assert.IsTrue(new PictureLocksComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void PictureLocksComparer_NoAdjustHandlesWrongTest()
+        {
+            var a = new PictureLocks() { NoAdjustHandles = true };
+            var b = new PictureLocks();
+            Assert.IsTrue(new PictureLocksComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void PictureLocksComparer_NoEditPointsWrongTest()
+        {
+            var a = new PictureLocks() { NoEditPoints = true };
+            var b = new PictureLocks();
+            Assert.IsTrue(new PictureLocksComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void PictureLocksComparer_NoResizeWrongTest()
+        {
+            var a = new PictureLocks() { NoResize = true };
+            var b = new PictureLocks();
+            Assert.IsTrue(new PictureLocksComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void PictureLocksComparer_NoMoveWrongTest()
+        {
+            var a = new PictureLocks() { NoMove = true };
+            var b = new PictureLocks();
+            Assert.IsTrue(new PictureLocksComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void PictureLocksComparer_NoRotationWrongTest()
+        {
+            var a = new PictureLocks() { NoRotation = true };
+            var b = new PictureLocks();
+            Assert.IsTrue(new PictureLocksComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void PictureLocksComparer_NoSelectionWrongTest()
+        {
+            var a = new PictureLocks() { NoSelection = true };
+            var b = new PictureLocks();
+            Assert.IsTrue(new PictureLocksComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void PictureLocksComparer_NoGroupingWrongTest()
+        {
+            var a = new PictureLocks() { NoGrouping = true };
+            var b = new PictureLocks();
+            Assert.IsTrue(new PictureLocksComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void PictureLocksComparer_NoChangeAspectWrongTest()
+        {
+            var a = new PictureLocks() { NoChangeAspect = true };
+            var b = new PictureLocks();
+            Assert.IsTrue(new PictureLocksComparer(a, b).Compare());
+        }
+        
     }
 }
--- OOXMLComparer/OOXMLComparer/Drawing/ShapePropertiesComparer.cs
+++ OOXMLComparer/OOXMLComparer/Drawing/ShapePropertiesComparer.cs
@@ -1,4 +1,5 @@
-﻿using OOXMLComparer.Helpers;
+﻿using DocumentFormat.OpenXml.Drawing;
+using OOXMLComparer.Helpers;
 
 namespace OOXMLComparer.Drawing
 {
@@ -11,6 +12,17 @@ namespace OOXMLComparer.Drawing
         public override bool Compare()
         {
             return a.CompareNullElements(b) ?? (Equals(a.BlackWhiteMode, b.BlackWhiteMode) && a.CompareChildren2(b));
+        }
+    }
+    public class PictureLocksComparer : OpenXmlElementComparer<PictureLocks>
+    {
+        public PictureLocksComparer(PictureLocks a, PictureLocks b) : base(a, b)
+        {
+        }
+
+        public override bool Compare()
+        {
+            return true;
         }
     }
 }
