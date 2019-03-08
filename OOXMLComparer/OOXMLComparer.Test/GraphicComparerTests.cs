71aba9a3eb4c1ea9c33f5ae11726f6f6513088b3
vladimir.morozov <vladimir.morozov@totalobjects.spb.ru>
2019-03-08 14:10:27 +0300

    Graphic compare

---
+++ OOXMLComparer/OOXMLComparer.Test/GraphicComparerTests.cs
@@ -0,0 +1,34 @@
+﻿using DocumentFormat.OpenXml.Drawing;
+using NUnit.Framework;
+using OOXMLComparer.Drawing;
+
+namespace OOXMLComparer.Test
+{
+    public class GraphicComparerTests
+    {
+        [Test]
+        public void GraphicComparerTest()
+        {
+            var a = new Graphic();
+            var b = new Graphic();
+            Assert.IsTrue(new GraphicComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void GraphicComparerWrongTest()
+        {
+            var a = new Graphic(
+                new GraphicData(new DocumentFormat.OpenXml.Drawing.Pictures.Picture(
+                                        new DocumentFormat.OpenXml.Drawing.Pictures.NonVisualPictureProperties(
+                                            new DocumentFormat.OpenXml.Drawing.Pictures.NonVisualDrawingProperties(),
+                                            new DocumentFormat.OpenXml.Drawing.Pictures.NonVisualPictureDrawingProperties(
+                                                new DocumentFormat.OpenXml.Drawing.PictureLocks() { NoChangeAspect = true, NoChangeArrowheads = true })
+                                            )
+                                        )
+                                    )
+                                   );
+            var b = new Graphic();
+            Assert.IsFalse(new GraphicComparer(a, b).Compare());
+        }
+    }
+}
---
+++ OOXMLComparer/OOXMLComparer/Drawing/GraphicComparer.cs
@@ -0,0 +1,17 @@
+﻿using DocumentFormat.OpenXml.Drawing;
+using OOXMLComparer.Helpers;
+
+namespace OOXMLComparer.Drawing
+{
+    public class GraphicComparer : OpenXmlElementComparer<Graphic>
+    {
+        public GraphicComparer(Graphic a, Graphic b) : base(a, b)
+        {
+        }
+
+        public override bool Compare()
+        {
+            return a.CompareChildren2(b);
+        }
+    }
+}
