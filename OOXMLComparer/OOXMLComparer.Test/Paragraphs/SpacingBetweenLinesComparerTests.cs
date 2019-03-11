5d790c6289bed17fc79a07e4f3bd78641f73cd2d
DwarfPorter <mrvlmor@gmail.com>
2019-03-11 23:33:12 +0300

    compare spacing

---
+++ OOXMLComparer/OOXMLComparer.Test/Paragraphs/SpacingBetweenLinesComparerTests.cs
@@ -0,0 +1,33 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using NUnit.Framework;
+using OOXMLComparer.Paragraphs;
+
+namespace OOXMLComparer.Test.Paragraphs
+{
+    public class SpacingBetweenLinesComparerTests
+    {
+        [Test]
+        public void SpacingBetweenLinesComparerTest()
+        {
+            var a = new SpacingBetweenLines();
+            var b = new SpacingBetweenLines();
+            Assert.IsTrue(new SpacingBetweenLinesComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void SpacingBetweenLinesComparerHappyTest()
+        {
+            var a = new SpacingBetweenLines() { After = "4", AfterAutoSpacing = true, AfterLines = 32, Before = "42", BeforeAutoSpacing = true, BeforeLines = 33, Line = "4", LineRule = LineSpacingRuleValues.AtLeast };
+            var b = new SpacingBetweenLines() { After = "4", AfterAutoSpacing = true, AfterLines = 32, Before = "42", BeforeAutoSpacing = true, BeforeLines = 33, Line = "4", LineRule = LineSpacingRuleValues.AtLeast };
+            Assert.IsTrue(new SpacingBetweenLinesComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void SpacingBetweenLinesComparerWrongTest()
+        {
+            var a = new SpacingBetweenLines() { After = "4", AfterAutoSpacing = true, AfterLines = 32, Before = "42", BeforeAutoSpacing = true, BeforeLines = 33, Line = "4", LineRule = LineSpacingRuleValues.AtLeast };
+            var b = new SpacingBetweenLines() { After = "4", AfterAutoSpacing = true, AfterLines = 32, Before = "42", BeforeAutoSpacing = true, BeforeLines = 33, Line = "4", LineRule = LineSpacingRuleValues.Auto };
+            Assert.IsFalse(new SpacingBetweenLinesComparer(a, b).Compare());
+        }
+    }
+}
---
+++ OOXMLComparer/OOXMLComparer/Paragraphs/SpacingBetweenLinesComparer.cs
@@ -0,0 +1,25 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using OOXMLComparer.Helpers;
+
+namespace OOXMLComparer.Paragraphs
+{
+    public class SpacingBetweenLinesComparer : OpenXmlElementComparer<SpacingBetweenLines>
+    {
+        public SpacingBetweenLinesComparer(SpacingBetweenLines a, SpacingBetweenLines b) : base(a, b)
+        {
+        }
+
+        public override bool Compare()
+        {
+            return a.CompareNullElements(b)
+                ?? Equals(a.After, b.After)
+                && Equals(a.AfterAutoSpacing, b.AfterAutoSpacing)
+                && Equals(a.AfterLines, b.AfterLines)
+                && Equals(a.Before, b.Before)
+                && Equals(a.BeforeAutoSpacing, b.BeforeAutoSpacing)
+                && Equals(a.BeforeLines, b.BeforeLines)
+                && Equals(a.Line, b.Line)
+                && Equals(a.LineRule, b.LineRule);
+        }
+    }
+}
