3c23efc10bab7160863a0971d701127282d9038f
vladimir.morozov <vladimir.morozov@totalobjects.spb.ru>
2019-03-12 13:46:00 +0300

    refactoring compare children ordered leaves

--- OOXMLComparer/OOXMLComparer/Paragraphs/ParagraphComparer.cs
+++ OOXMLComparer/OOXMLComparer/Paragraphs/ParagraphComparer.cs
@@ -13,18 +13,7 @@ namespace OOXMLComparer.Paragraphs
 
         public override bool Compare()
         {
-            var answer = a.CompareNullElements(b);
-            if (answer != null)
-            {
-                return answer.Value;
-            }
-            var propComparer = new ParagraphPropertiesComparer(a.ParagraphProperties, b.ParagraphProperties);
-            var answer2 = propComparer.Compare();
-            if (!answer2)
-            {
-                return answer2;
-            }
-            return OpenXmlComparerHelper.CompareOrderedChildren(a.ChildElements.Where(p => p.GetType() != typeof(ParagraphProperties)), b.ChildElements.Where(p => p.GetType() != typeof(ParagraphProperties)));
+            return a.CompareOrderedChildren(b, t => ((Paragraph)t).ParagraphProperties);
         }
     }
 }
