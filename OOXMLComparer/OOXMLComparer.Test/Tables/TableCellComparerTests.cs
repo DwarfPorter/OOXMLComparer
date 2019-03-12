c93052d7adb33dd730a12499fff783482e1779df
vladimir.morozov <vladimir.morozov@totalobjects.spb.ru>
2019-03-12 13:54:25 +0300

    Table Cell comparer

---
+++ OOXMLComparer/OOXMLComparer.Test/Tables/TableCellComparerTests.cs
@@ -0,0 +1,33 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using NUnit.Framework;
+using OOXMLComparer.Tables;
+
+namespace OOXMLComparer.Test.Tables
+{
+    public class TableCellComparerTests
+    {
+        [Test]
+        public void TableCellComparerTest()
+        {
+            var a = new TableCell();
+            var b = new TableCell();
+            Assert.IsTrue(new TableCellComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void TableCellComparerHappyTest()
+        {
+            var a = new TableCell(new TableCellProperties { NoWrap = new NoWrap { Val = OnOffOnlyValues.On } }, new Run(new Text("aaa")));
+            var b = new TableCell(new Run(new Text("aaa"))) { TableCellProperties = new TableCellProperties { NoWrap = new NoWrap { Val = OnOffOnlyValues.On } } };
+            Assert.IsTrue(new TableCellComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void TableCellComparerWrongTest()
+        {
+            var a = new TableCell(new TableCellProperties { NoWrap = new NoWrap { Val = OnOffOnlyValues.On } }, new Run(new Text("bbb")), new Run(new Text("aaa")));
+            var b = new TableCell(new Run(new Text("aaa")), new Run(new Text("bbb"))) { TableCellProperties = new TableCellProperties { NoWrap = new NoWrap { Val = OnOffOnlyValues.On } } };
+            Assert.IsFalse(new TableCellComparer(a, b).Compare());
+        }
+    }
+}
---
+++ OOXMLComparer/OOXMLComparer/Tables/TableCellComparer.cs
@@ -0,0 +1,17 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using OOXMLComparer.Helpers;
+
+namespace OOXMLComparer.Tables
+{
+    public class TableCellComparer : OpenXmlElementComparer<TableCell>
+    {
+        public TableCellComparer(TableCell a, TableCell b) : base(a, b)
+        {
+        }
+
+        public override bool Compare()
+        {
+            return a.CompareOrderedChildren(b, t => ((TableCell)t).TableCellProperties);
+        }
+    }
+}
