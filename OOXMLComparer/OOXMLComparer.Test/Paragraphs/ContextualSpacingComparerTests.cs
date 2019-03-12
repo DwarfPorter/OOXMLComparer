1e5ef0844438cc34768cbd2a108dec393b977ffc
vladimir.morozov <vladimir.morozov@totalobjects.spb.ru>
2019-03-12 09:18:18 +0300

    ContextualSpacing check

---
+++ OOXMLComparer/OOXMLComparer.Test/Paragraphs/ContextualSpacingComparerTests.cs
@@ -0,0 +1,25 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using NUnit.Framework;
+using OOXMLComparer.Paragraphs.ParaProperties;
+
+namespace OOXMLComparer.Test.Paragraphs
+{
+    public class ContextualSpacingComparerTests
+    {
+        [Test]
+        public void KeepLinesComparerTest()
+        {
+            var a = new ContextualSpacing();
+            var b = new ContextualSpacing();
+            Assert.IsTrue(new ContextualSpacingComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void KeepLinesComparerWrongTest()
+        {
+            var a = new ContextualSpacing() { Val = true };
+            var b = new ContextualSpacing();
+            Assert.IsFalse(new ContextualSpacingComparer(a, b).Compare());
+        }
+    }
+}
---
+++ OOXMLComparer/OOXMLComparer/Paragraphs/ParaProperties/ContextualSpacingComparer.cs
@@ -0,0 +1,11 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace OOXMLComparer.Paragraphs.ParaProperties
+{
+    public class ContextualSpacingComparer : OnOffComparer
+    {
+        public ContextualSpacingComparer(ContextualSpacing a, ContextualSpacing b) : base(a, b)
+        {
+        }
+    }
+}
