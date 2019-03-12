a8a55c23178398e898cd6d02f7961f543ae70535
vladimir.morozov <vladimir.morozov@totalobjects.spb.ru>
2019-03-12 15:14:10 +0300

    Table justification comparer

---
+++ OOXMLComparer/OOXMLComparer.Test/Tables/TableProperties/TableJustificationComparerTests.cs
@@ -0,0 +1,33 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using NUnit.Framework;
+using OOXMLComparer.Tables.TableProperties;
+
+namespace OOXMLComparer.Test.Tables.TableProperties
+{
+    public class TableJustificationComparerTests
+    {
+        [Test]
+        public void TableJustificationComparerTest()
+        {
+            var a = new TableJustification();
+            var b = new TableJustification();
+            Assert.IsTrue(new TableJustificationComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void TableJustificationComparerHappyTest()
+        {
+            var a = new TableJustification() { Val = TableRowAlignmentValues.Center };
+            var b = new TableJustification() { Val = TableRowAlignmentValues.Center };
+            Assert.IsTrue(new TableJustificationComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void TableJustificationComparerWrongTest()
+        {
+            var a = new TableJustification() { Val = TableRowAlignmentValues.Center };
+            var b = new TableJustification() { Val = TableRowAlignmentValues.Left };
+            Assert.IsFalse(new TableJustificationComparer(a, b).Compare());
+        }
+    }
+}
---
+++ OOXMLComparer/OOXMLComparer/Tables/TableProperties/TableJustificationComparer.cs
@@ -0,0 +1,20 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using OOXMLComparer.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOXMLComparer.Tables.TableProperties
+{
+    public class TableJustificationComparer : OpenXmlElementComparer<TableJustification>
+    {
+        public TableJustificationComparer(TableJustification a, TableJustification b) : base(a, b)
+        {
+        }
+
+        public override bool Compare()
+        {
+            return a.CompareNullElements(b) ?? Equals(a.Val, b.Val);
+        }
+    }
+}
