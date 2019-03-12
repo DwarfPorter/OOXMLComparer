ffb88b4e54eb7d5cc69e6e97deafec8834679581
vladimir.morozov <vladimir.morozov@totalobjects.spb.ru>
2019-03-12 17:02:39 +0300

    TableIdentation

---
+++ OOXMLComparer/OOXMLComparer.Test/Tables/TableProperties/TableIndentationComparerTests.cs
@@ -0,0 +1,41 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using NUnit.Framework;
+using OOXMLComparer.Tables.TableProperties;
+
+namespace OOXMLComparer.Test.Tables.TableProperties
+{
+    public class TableIndentationComparerTests
+    {
+        [Test]
+        public void TableIndentationComparerTest()
+        {
+            var a = new TableIndentation();
+            var b = new TableIndentation();
+            Assert.IsTrue(new TableIndentationComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void TableIndentationComparerHappyTest()
+        {
+            var a = new TableIndentation() { Type = TableWidthUnitValues.Dxa, Width = 33 };
+            var b = new TableIndentation() { Type = TableWidthUnitValues.Dxa, Width = 33 };
+            Assert.IsTrue(new TableIndentationComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void TableIndentationComparerWidhtWrongTest()
+        {
+            var a = new TableIndentation() { Type = TableWidthUnitValues.Dxa, Width = 30 };
+            var b = new TableIndentation() { Type = TableWidthUnitValues.Dxa, Width = 33 };
+            Assert.IsFalse(new TableIndentationComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void TableIndentationComparerTypeWrongTest()
+        {
+            var a = new TableIndentation() { Type = TableWidthUnitValues.Dxa, Width = 33 };
+            var b = new TableIndentation() { Type = TableWidthUnitValues.Pct, Width = 33 };
+            Assert.IsFalse(new TableIndentationComparer(a, b).Compare());
+        }
+    }
+}
---
+++ OOXMLComparer/OOXMLComparer/Tables/TableProperties/TableIndentationComparer.cs
@@ -0,0 +1,19 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using OOXMLComparer.Helpers;
+
+namespace OOXMLComparer.Tables.TableProperties
+{
+    public class TableIndentationComparer : OpenXmlElementComparer<TableIndentation>
+    {
+        public TableIndentationComparer(TableIndentation a, TableIndentation b) : base(a, b)
+        {
+        }
+
+        public override bool Compare()
+        {
+            return a.CompareNullElements(b)
+                ?? Equals(a.Type, b.Type)
+                && Equals(a.Width, b.Width);
+        }
+    }
+}
