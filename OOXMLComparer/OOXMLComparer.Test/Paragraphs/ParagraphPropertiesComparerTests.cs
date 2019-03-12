8d2e8edf2678ed63ee0783d8352710a94d54099d
vladimir.morozov <vladimir.morozov@totalobjects.spb.ru>
2019-03-12 09:27:14 +0300

    compare paragraph properties

---
+++ OOXMLComparer/OOXMLComparer.Test/Paragraphs/ParagraphPropertiesComparerTests.cs
@@ -0,0 +1,33 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using NUnit.Framework;
+using OOXMLComparer.Paragraphs.ParaProperties;
+
+namespace OOXMLComparer.Test.Paragraphs
+{
+    public class ParagraphPropertiesComparerTests
+    {
+        [Test]
+        public void ParagraphPropertiesComparerTest()
+        {
+            var a = new ParagraphProperties();
+            var b = new ParagraphProperties();
+            Assert.IsTrue(new ParagraphPropertiesComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void ParagraphPropertiesComparerHappyTest()
+        {
+            var a = new ParagraphProperties(new TextDirection { Val = TextDirectionValues.BottomToTopLeftToRight });
+            var b = new ParagraphProperties(new TextDirection { Val = TextDirectionValues.BottomToTopLeftToRight });
+            Assert.IsTrue(new ParagraphPropertiesComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void ParagraphPropertiesComparerWrongTest()
+        {
+            var a = new ParagraphProperties(new TextDirection { Val = TextDirectionValues.BottomToTopLeftToRight });
+            var b = new ParagraphProperties();
+            Assert.IsFalse(new ParagraphPropertiesComparer(a, b).Compare());
+        }
+    }
+}
---
+++ OOXMLComparer/OOXMLComparer/Paragraphs/ParaProperties/ParagraphPropertiesComparer.cs
@@ -0,0 +1,17 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using OOXMLComparer.Helpers;
+
+namespace OOXMLComparer.Paragraphs.ParaProperties
+{
+    public class ParagraphPropertiesComparer : OpenXmlElementComparer<ParagraphProperties>
+    {
+        public ParagraphPropertiesComparer(ParagraphProperties a, ParagraphProperties b) : base(a, b)
+        {
+        }
+
+        public override bool Compare()
+        {
+            return a.CompareChildren2(b);
+        }
+    }
+}
