1c89edc8e6b58c4ccdb84729ed5736a30b2a0e7c
vladimir.morozov <vladimir.morozov@totalobjects.spb.ru>
2019-03-13 16:58:40 +0300

    TableGridComparer check (prepare for table)

---
+++ OOXMLComparer/OOXMLComparer.Test/Tables/TableGridComparerTests.cs
@@ -0,0 +1,25 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using NUnit.Framework;
+using OOXMLComparer.Tables;
+
+namespace OOXMLComparer.Test.Tables
+{
+    public class TableGridComparerTests
+    {
+        [Test]
+        public void TableGridComparerTest()
+        {
+            var a = new TableGrid();
+            var b = new TableGrid();
+            Assert.IsTrue(new TableGridComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void TableGridComparerWrongTest()
+        {
+            var a = new TableGrid(new GridColumn { Width = "50" }, new GridColumn { Width = "42" });
+            var b = new TableGrid(new GridColumn { Width = "42" }, new GridColumn { Width = "50" });
+            Assert.IsFalse(new TableGridComparer(a, b).Compare());
+        }
+    }
+}
---
+++ OOXMLComparer/OOXMLComparer/Tables/TableGridComparer.cs
@@ -0,0 +1,19 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using OOXMLComparer.Helpers;
+
+namespace OOXMLComparer.Tables
+{
+    public class TableGridComparer : OpenXmlElementComparer<TableGrid>
+    {
+        public TableGridComparer(TableGrid a, TableGrid b) : base(a, b)
+        {
+        }
+
+        public override bool Compare()
+        {
+            return a.CompareNullElements(b)
+                ?? a.ChildElements.CompareOrderedChildren(b.ChildElements);
+        }
+    }
+}
