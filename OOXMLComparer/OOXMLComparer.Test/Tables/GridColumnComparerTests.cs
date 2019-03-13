2320577fe6b11bd4a8550294cbddce428b2c2fad
vladimir.morozov <vladimir.morozov@totalobjects.spb.ru>
2019-03-13 12:11:35 +0300

    GridColumn check (prepare for TableGrid)

---
+++ OOXMLComparer/OOXMLComparer.Test/Tables/GridColumnComparerTests.cs
@@ -0,0 +1,25 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using NUnit.Framework;
+using OOXMLComparer.Tables;
+
+namespace OOXMLComparer.Test.Tables
+{
+    public class GridColumnComparerTests
+    {
+        [Test]
+        public void GridColumnComparerTest()
+        {
+            var a = new GridColumn();
+            var b = new GridColumn();
+            Assert.IsTrue(new GridColumnComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void GridColumnComparerWrongTest()
+        {
+            var a = new GridColumn() { Width = "30" };
+            var b = new GridColumn();
+            Assert.IsFalse(new GridColumnComparer(a, b).Compare());
+        }
+    }
+}
---
+++ OOXMLComparer/OOXMLComparer/Tables/GridColumnComparer.cs
@@ -0,0 +1,17 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using OOXMLComparer.Helpers;
+
+namespace OOXMLComparer.Tables
+{
+    public class GridColumnComparer : OpenXmlElementComparer<GridColumn>
+    {
+        public GridColumnComparer(GridColumn a, GridColumn b) : base(a, b)
+        {
+        }
+
+        public override bool Compare()
+        {
+            return a.CompareNullElements(b) ?? Equals(a.Width, b.Width);
+        }
+    }
+}
