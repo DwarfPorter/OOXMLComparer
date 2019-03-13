819ee69b135778da26f9195eeca6dffc1687bee9
vladimir.morozov <vladimir.morozov@totalobjects.spb.ru>
2019-03-13 08:49:09 +0300

    Check TableLayout

---
+++ OOXMLComparer/OOXMLComparer.Test/Tables/TableProperties/TableLayoutComparerTests.cs
@@ -0,0 +1,38 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using NUnit.Framework;
+using OOXMLComparer.Tables.TableProperties;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOXMLComparer.Test.Tables.TableProperties
+{
+    public class TableLayoutComparerTests
+    {
+        [Test]
+        public void TableLayoutComparerTest()
+        {
+            var a = new TableLayout();
+            var b = new TableLayout();
+            Assert.IsTrue(new TableLayoutComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void TableLayoutComparerWrongTest()
+        {
+            var a = new TableLayout() { Type = TableLayoutValues.Fixed };
+            var b = new TableLayout();
+            Assert.IsFalse(new TableLayoutComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void TableLayoutComparerDefaultTest()
+        {
+            var a = new TableLayout() { Type = TableLayoutValues.Autofit };
+            var b = new TableLayout();
+            Assert.IsTrue(new TableLayoutComparer(a, b).Compare());
+            Assert.IsTrue(new TableLayoutComparer(a, null).Compare());
+        }
+
+    }
+}
---
+++ OOXMLComparer/OOXMLComparer/Tables/TableProperties/TableLayoutComparer.cs
@@ -0,0 +1,19 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using OOXMLComparer.Helpers;
+
+namespace OOXMLComparer.Tables.TableProperties
+{
+    public class TableLayoutComparer : OpenXmlElementComparer<TableLayout>
+    {
+        public TableLayoutComparer(TableLayout a, TableLayout b) : base(a, b)
+        {
+        }
+
+        public override bool Compare()
+        {
+            return a.CompareNullElements(b, t => t.Type == TableLayoutValues.Autofit) 
+                ?? Equals(a.Type ?? TableLayoutValues.Autofit, b.Type ?? TableLayoutValues.Autofit);
+        }
+    }
+}
