203841a90b2e29b48a79e7a17de6512f06dc946d
vladimir.morozov <vladimir.morozov@totalobjects.spb.ru>
2019-03-13 09:31:13 +0300

    TableWidthComparer check

---
+++ OOXMLComparer/OOXMLComparer.Test/Tables/TableProperties/TableWidthComparerTests.cs
@@ -0,0 +1,33 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using NUnit.Framework;
+using OOXMLComparer.Tables.TableProperties;
+
+namespace OOXMLComparer.Test.Tables.TableProperties
+{
+    public class TableWidthComparerTests
+    {
+        [Test]
+        public void TableWidthComparerTest()
+        {
+            var a = new TableWidth();
+            var b = new TableWidth();
+            Assert.IsTrue(new TableWidthComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void TableWidthComparerTypeWrongTest()
+        {
+            var a = new TableWidth() { Type = TableWidthUnitValues.Dxa };
+            var b = new TableWidth() { Type = TableWidthUnitValues.Pct };
+            Assert.IsFalse(new TableWidthComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void TableWidthComparerWidthWrongTest()
+        {
+            var a = new TableWidth() { Width = "20" };
+            var b = new TableWidth();
+            Assert.IsFalse(new TableWidthComparer(a, b).Compare());
+        }
+    }
+}
---
+++ OOXMLComparer/OOXMLComparer/Tables/TableProperties/TableWidthComparer.cs
@@ -0,0 +1,17 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using OOXMLComparer.Helpers;
+
+namespace OOXMLComparer.Tables.TableProperties
+{
+    public class TableWidthComparer : OpenXmlElementComparer<TableWidth>
+    {
+        public TableWidthComparer(TableWidth a, TableWidth b) : base(a, b)
+        {
+        }
+
+        public override bool Compare()
+        {
+            return a.CompareNullElements(b) ?? Equals(a.Type, b.Type) && Equals(a.Width, b.Width);
+        }
+    }
+}
