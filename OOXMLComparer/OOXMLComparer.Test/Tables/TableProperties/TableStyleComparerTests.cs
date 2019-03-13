a4b2582e122b4f70298762e8b12f74d20e1be068
vladimir.morozov <vladimir.morozov@totalobjects.spb.ru>
2019-03-13 09:23:30 +0300

    TableStyle check

---
+++ OOXMLComparer/OOXMLComparer.Test/Tables/TableProperties/TableStyleComparerTests.cs
@@ -0,0 +1,25 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using NUnit.Framework;
+using OOXMLComparer.Tables.TableProperties;
+
+namespace OOXMLComparer.Test.Tables.TableProperties
+{
+    public class TableStyleComparerTests
+    {
+        [Test]
+        public void TableStyleComparerTest()
+        {
+            var a = new TableStyle();
+            var b = new TableStyle();
+            Assert.IsTrue(new TableStyleComparer(a, b).Compare());
+        }
+
+        [Test]
+        public void TableStyleComparerWrongTest()
+        {
+            var a = new TableStyle() { Val = "aa" };
+            var b = new TableStyle();
+            Assert.IsFalse(new TableStyleComparer(a, b).Compare());
+        }
+    }
+}
---
+++ OOXMLComparer/OOXMLComparer/Tables/TableProperties/TableStyleComparer.cs
@@ -0,0 +1,17 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using OOXMLComparer.Helpers;
+
+namespace OOXMLComparer.Tables.TableProperties
+{
+    public class TableStyleComparer : OpenXmlElementComparer<TableStyle>
+    {
+        public TableStyleComparer(TableStyle a, TableStyle b) : base(a, b)
+        {
+        }
+
+        public override bool Compare()
+        {
+            return a.CompareNullElements(b) ?? Equals(a.Val, b.Val);
+        }
+    }
+}
